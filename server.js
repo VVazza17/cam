243b2cd217073de074e52195f23c032250266b2a
Kyle Deng <kyle.deng01@gmail.com>
2025-01-01 21:51:17 -0500

    Serve index.html

--- server.js
+++ server.js
@@ -2,8 +2,10 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
-	res.send('Live Streaming Server is running.');
+	res.sendFile(__dirname + '/index.html');
 });
 	
 app.listen(PORT, () => {
